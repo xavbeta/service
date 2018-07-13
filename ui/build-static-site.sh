0653d11c8dd400edacb367df245c5fd0c27f0a53
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-07-13 10:31:55 +0200

    Switch to klakegg/hugo:0.43-ext-alpine image for Hugo 0.43

--- ui/build-static-site.sh
+++ ui/build-static-site.sh
@@ -5,5 +5,5 @@ echo "Retrieving website source…"
 git clone https://github.com/SmartRoadSense/website.git /tmp/repository
 mv /tmp/repository/hugo/* /src
 
-echo "Building website…"
-hugo --source="/src" --destination="/output" --baseURL="$HUGO_BASEURL" "$@" || exit 1
+# Build website
+hugo --source="/src" --destination="/target" --baseURL="$HUGO_BASEURL" "$@" || exit 1
